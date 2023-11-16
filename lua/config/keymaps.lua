472fe8000db7baa0ddf9f7cbc4b2d9a526a00bef
Bilgehan Sel <bilgehansel@gmail.com>
2023-11-16 09:47:05 -0500

    first changes to LazyVim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- My Custom Keymaps
+local keymap = vim.keymap
+
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("x", "<leader>p", '"_dP')
+keymap.set("n", "Q", "<nop>")
---
+++ lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.hlsearch = true
+opt.incsearch = true
+
+opt.scrolloff = 6
+
+opt.updatetime = 50
+
+opt.colorcolumn = "80"
